4e528d9e4522f1a5ef57fd3e5b322ba7901f2863
ad <ad@itmp.top>
2017-06-22 14:38:42 +0800

    third

--- detect_proxy.py
+++ detect_proxy.py
@@ -1,8 +1,9 @@
-#-*- coding: UTF-8 -*-
+#-*- coding:UTF-8 -*-
 import urllib2
-
+import random
+ip_list = ['119.6.136.122', '114.106.77.14']
 url = 'http://www.ip181.com/'
-proxy_support = urllib2.ProxyHandler({'http': '121.40.108.76'})
+proxy_support = urllib2.ProxyHandler({'https':random.choice(ip_list)})
 opener = urllib2.build_opener(proxy_support)
 opener.add_handler = [('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36')]
 urllib2.install_opener(opener)
